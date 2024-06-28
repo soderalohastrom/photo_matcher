faa4ae27cd6f9bfbdb3761a321f6534132b64e81
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-28 13:47:34 -1000

    still trying for dlib

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install build essentials and CMake
+apt-get update && apt-get install -y cmake build-essential
+
 # Install Python dependencies
 pip install -r requirements.txt
 
 # Additional commands to build dlib if necessary
-pip install cmake
 pip install dlib --no-cache-dir
 pip install face_recognition
