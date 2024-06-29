2011668910788770f7107ff481ea4867253fd2dc
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-28 15:24:25 -1000

    upadte per new info

--- build.sh
+++ build.sh
@@ -2,12 +2,28 @@
 # exit on error
 set -o errexit
 
+# Print Python version
+python --version
+
+# Update package lists
+sudo apt-get update
+
 # Install build essentials and CMake
-apt-get update && apt-get install -y cmake build-essential
+sudo apt-get install -y build-essential cmake
+
+# Install OpenBLAS and LAPACK for optimizations
+sudo apt-get install -y libopenblas-dev liblapack-dev 
+
+# We don't need to install Python as it's already available in the Render environment
+# But we'll make sure pip is up to date
+pip install --upgrade pip
 
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Additional commands to build dlib if necessary
-pip install dlib --no-cache-dir
+# Install dlib
+DLIB_VERSION=19.24.0
+pip install dlib==${DLIB_VERSION} --no-cache-dir
+
+# Install face_recognition
 pip install face_recognition
