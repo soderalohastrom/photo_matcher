95969bbdfe22e70f80d11ba4127df723ec1b1e15
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-28 16:04:20 -1000

    again

--- build.sh
+++ build.sh
@@ -1,42 +1,29 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Print Python version
 python --version
-
-# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Try to install dlib using different methods
 install_dlib() {
-    # Method 1: Try to install the specified version
+    # Try pip install first
     pip install dlib==19.24.0 --no-cache-dir && return 0
 
-    # Method 2: Try to install the latest version
-    pip install dlib --no-cache-dir && return 0
-
-    # Method 3: Try to install a pre-built wheel
-    pip install https://github.com/jloh02/dlib/releases/download/v19.22/dlib-19.22.99-cp311-cp311-linux_x86_64.whl && return 0
-
-    # Method 4: Build from source
-    git clone https://github.com/davisking/dlib.git
-    cd dlib
+    # If pip fails, build from source
+    wget http://dlib.net/files/dlib-19.24.tar.bz2
+    tar xvf dlib-19.24.tar.bz2
+    cd dlib-19.24/
     mkdir build
     cd build
     cmake ..
-    cmake --build .
+    cmake --build . --config Release
     cd ..
     python setup.py install
     cd ..
-    rm -rf dlib
+    rm -rf dlib-19.24 dlib-19.24.tar.bz2
 }
 
-# Attempt to install dlib
 install_dlib || echo "Failed to install dlib"
-
-# Install face_recognition
 pip install face_recognition
 
 echo "Build script completed"
