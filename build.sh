1571ff62e785f4f0364ca3b981f2f7c3c4849788
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-28 16:50:00 -1000

    dependencies

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Ensure we're using Python 3.10
+pyenv install 3.10.9
+pyenv global 3.10.9
+
 python --version
 pip install --upgrade pip
 pip install -r requirements.txt
