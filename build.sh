3f24e273bf2dabc34f707b105d76d2cede77d910
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-28 16:53:36 -1000

    again

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Ensure we're using Python 3.10
-pyenv install 3.10.9
-pyenv global 3.10.9
-
+# Print Python version (should be set by Render.com)
 python --version
+
+# Upgrade pip
 pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
 echo "Build script completed"
