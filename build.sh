b2cb350395404a0aa44876c39390c497406fbe8c
Scott Soderstrom <ssoderstrom@gmail.com>
2024-06-28 15:27:33 -1000

    again

--- build.sh
+++ build.sh
@@ -5,20 +5,8 @@ set -o errexit
 # Print Python version
 python --version
 
-# Update package lists
-sudo apt-get update
-
-# Install build essentials and CMake
-sudo apt-get install -y build-essential cmake
-
-# Install OpenBLAS and LAPACK for optimizations
-sudo apt-get install -y libopenblas-dev liblapack-dev 
-
-# We don't need to install Python as it's already available in the Render environment
-# But we'll make sure pip is up to date
-pip install --upgrade pip
-
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Install dlib
